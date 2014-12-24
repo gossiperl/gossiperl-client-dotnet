30bede093b4a1ff0a6b876ebd2e86ca2d8684837
radekg <radek@gruchalski.com>
2014-12-24 03:21:25 +0100

    Initial commit of the .net client.

---
+++ exec/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gossiperl.Client;
+
+namespace gossiperlclientdotnetexec
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Supervisor s = new Supervisor ();
+		}
+	}
+}
---
+++ lib/Supervisor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gossiperl.Client
+{
+	public class Supervisor
+	{
+		public void connect() {
+		}
+
+		public void disconnect() {
+		}
+	}
+}
+
